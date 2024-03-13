1da386f39c56ace026455ae860cc20926b553137
Gustavo Almeida <gstvcrralmeida@gmail.com>
2024-03-13 19:44:37 -0300

    feat: implements validate customer

--- src/main/java/br/com/fatecrestapi/FoodFatec/service/CustomerService.java
+++ src/main/java/br/com/fatecrestapi/FoodFatec/service/CustomerService.java
@@ -5,7 +5,6 @@ import br.com.fatecrestapi.FoodFatec.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
@@ -20,20 +19,46 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public List<Customer> getInfoCustomers(){
-        return customerRepository.findAll();
-    }
-    public Customer saveCustomer(Customer customer) {
+   /* private void validarCustomer(Customer customer){
         if (customer.getMonthlyIncomeCustomer() != null &&
                 customer.getMonthlyIncomeCustomer().compareTo(BigDecimal.valueOf(0)) >= 0 &&
                 !customer.getPasswordCustomer().equals("") &&
                 customer.getPasswordCustomer() != null) {
+
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "A renda mensal do cliente é " +
+                            "obrigatória e deve ser maior ou igual a 0 (zero)!");
+        }
+    }
+*/
+    private Boolean validarCustomer(Customer customer){
+        if (customer.getMonthlyIncomeCustomer() != null &&
+                customer.getMonthlyIncomeCustomer().compareTo(BigDecimal.valueOf(0)) >= 0 &&
+                !customer.getPasswordCustomer().equals("") &&
+                customer.getPasswordCustomer() != null) {
+
+            return  true;
+
+        } else {
+            return false;
+        }
+    }
+
+
+    public List<Customer> getInfoCustomers(){
+        return customerRepository.findAll();
+    }
+    public Customer saveCustomer(Customer customer) {
+        if (validarCustomer(customer)) {
             return customerRepository.saveAndFlush(customer);
         } else {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
                     "A renda mensal do cliente é " +
                             "obrigatória e deve ser maior ou igual a 0 (zero)!");
         }
+
+
     }
     public HashMap<String,Object> deleteCustomer(Long idCustomer){
         Optional<Customer> customer =
@@ -56,21 +81,15 @@ public class CustomerService {
     }
 
    public Customer updateCustomer(Customer customer){
-        if(customer.getMonthlyIncomeCustomer() != null &&
-                customer.getMonthlyIncomeCustomer().compareTo(BigDecimal.valueOf(0)) >=0 &&
-          !customer.getPasswordCustomer().equals("") &&
-                customer.getPasswordCustomer() != null){
+        validarCustomer(customer);
               if(findCustomerByID(customer.getIdCustomer()) != null){
                   return customerRepository.saveAndFlush(customer);
               }else {
                   throw new ResponseStatusException(HttpStatus.NOT_FOUND,
                           "cliente não encontrado");
               }
-            }else {
-              throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                      "A renda salarial deve ser maior ou igual 0!");
-        }
+            }
 
    }
 
-}
+
